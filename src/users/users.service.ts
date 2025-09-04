baed6fbd1c5096c2b3b4195db60f7e11bdb6499b
Maria Luna <mlunat20@gmail.com>
2025-09-04 16:09:05 -0500

    add services refactor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,94 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
+import { Response, User } from './user.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: '1',
+      name: 'Maria Luna',
+      email: 'maria.luna@example.com',
+    },
+    {
+      id: '2',
+      name: 'Carmen Lopez',
+      email: 'carmen.lopez@example.com',
+    },
+    {
+      id: '3',
+      name: 'Vero Cornejo',
+      email: 'vero.cornejo@example.com',
+    },
+  ];
+
+  findAll(): Response {
+    const response: Response = {
+      message: 'Users found successfully',
+    };
+
+    response.data = this.users;
+
+    return response;
+  }
+
+  findOne(id: string): Response {
+    const result = this.users.find((user) => user.id === id);
+
+    const response: Response = {
+      message: 'User found successfully',
+    };
+
+    if (!result) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    response.data = result;
+
+    return response;
+  }
+
+  create(body: CreateUserDto): Response {
+    this.users.push({
+      ...body,
+      id: randomUUID(),
+    });
+
+    return {
+      message: 'User created successfully',
+    };
+  }
+
+  delete(id: string): Response {
+    const position = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    this.users.splice(position, 1);
+
+    return {
+      message: 'User deleted successfully',
+    };
+  }
+
+  update(id: string, body: UpdateUserDto): Response {
+    const position = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    const updatedUser = {
+      ...this.users[position],
+      ...body,
+    };
+
+    this.users[position] = updatedUser;
+
+    return {
+      message: 'User updated successfully',
+      data: this.users[position],
+    };
+  }
+}
