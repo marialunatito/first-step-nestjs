fd122c9d558d6c2ad437e647628e6b618b939cf5
Maria Luna <mlunat20@gmail.com>
2025-09-06 18:16:35 -0500

    update user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Env } from 'env.model';
 import { CreateUserDto, UpdateUserDto } from './user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private readonly configService: ConfigService<Env>,
+  ) {}
 
   @Get()
   findUsers() {
+    const port = this.configService.get('PORT', { infer: true });
+    console.log('port controller: ', port);
     return this.userService.findAll();
   }
 
