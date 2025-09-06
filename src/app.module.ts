d9b8cbbe62a85d31de949ae1975ca147fa5e1cce
Maria Luna <mlunat20@gmail.com>
2025-09-06 00:59:50 -0500

    add envs configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { Env } from 'env.model';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot<Env>()],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
