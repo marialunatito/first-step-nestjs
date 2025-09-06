1cb0090cccc656486e27f57d19fef2772759084b
Maria Luna <mlunat20@gmail.com>
2025-09-06 18:16:26 -0500

    refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { Env } from 'env.model';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot<Env>()],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
