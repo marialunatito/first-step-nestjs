fed028e8ef6cc1d64ced82bb75b6bac371102099
Maria Luna <mlunat20@gmail.com>
2025-09-09 18:52:24 -0500

    add module users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from './entity/profile.entity';
+import { User } from './entity/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
