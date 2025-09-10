e44e6c198280852d62eeecc6c3c4be62b65c48d2
Maria Luna <mlunat20@gmail.com>
2025-09-10 17:43:18 -0500

    app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Env } from 'env.model';
-import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
