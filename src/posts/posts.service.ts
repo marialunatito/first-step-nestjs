b59d75f2cee89817d7f498bb760bae4764c53e32
Maria Luna <mlunat20@gmail.com>
2025-09-10 14:57:17 -0500

    add logic service posts for crud

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    try {
+      return await this.postsRepository
+        .save({
+          ...createPostDto,
+          user: { id: createPostDto.userId },
+        })
+        .catch();
+    } catch {
+      throw new BadRequestException(`Error creating post`);
+    }
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.postsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const posts = await this.postsRepository.findOneBy({ id });
+    if (!posts) {
+      throw new NotFoundException(`Post ${id} not found`);
+    }
+
+    return posts;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    try {
+      const post = await this.postsRepository.findOneBy({ id });
+
+      if (!post) {
+        throw new NotFoundException(`Post ${id} not found`);
+      }
+
+      const postMerge = this.postsRepository.merge(post, updatePostDto);
+      const savePost = await this.postsRepository.save(postMerge);
+      return savePost;
+    } catch {
+      throw new BadRequestException(`Error update post`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  deleted(id: number) {
+    return this.postsRepository.delete({ id });
   }
 }
