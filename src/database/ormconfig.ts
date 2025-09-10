699512e0d54b2b9de2d0c56435972f6666f7c303
Maria Luna <mlunat20@gmail.com>
2025-09-10 18:58:11 -0500

    add config data base for orm

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['./src/**/*.entity.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  synchronize: false,
+});
