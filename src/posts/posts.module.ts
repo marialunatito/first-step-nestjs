0bfcc20825ced61c1457b687ce8aec1d2d4563e4
Maria Luna <mlunat20@gmail.com>
2025-09-10 14:57:26 -0500

    add configuration post module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entity/user.entity';
+import { Post } from './entities/post.entity';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, User])],
   controllers: [PostsController],
   providers: [PostsService],
+  exports: [PostsService],
 })
 export class PostsModule {}
