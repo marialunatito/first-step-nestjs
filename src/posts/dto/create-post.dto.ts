f094a98b8ec10385a1e61598776a950d459f0eb4
Maria Luna <mlunat20@gmail.com>
2025-09-10 14:57:04 -0500

    add dto create post

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,27 @@
-export class CreatePostDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  content: string;
+
+  @IsString()
+  @IsOptional()
+  coverImage?: string;
+
+  @IsString()
+  @IsOptional()
+  summary?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isDraft?: boolean;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+}
