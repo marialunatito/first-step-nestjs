ba519e0387b2c5dae30ee68300aff3dd8d8b9aff
Maria Luna <mlunat20@gmail.com>
2025-09-12 11:26:58 -0500

    add configuration of jwt in module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'my-secret-key',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
