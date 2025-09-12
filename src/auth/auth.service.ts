2f7a7e8cc13ab60ed2a5512dc3c0c910e29b0920
Maria Luna <mlunat20@gmail.com>
2025-09-12 11:01:15 -0500

    fix type validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from 'src/users/users.service';
+import { User } from '../users/entity/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersRepository: UsersService) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.usersRepository.getUserByEmail(email);
     if (!user) {
       throw new UnauthorizedException(`User not Unauthorized`);
